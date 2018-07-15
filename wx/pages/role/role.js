6530eeb1f7cae61cff0be36f5992c77a64bb25aa
myloxli <myloxli@tencent.com>
2018-07-15 17:17:30 +0800

    improve role selection

--- wx/pages/role/role.js
+++ wx/pages/role/role.js
@@ -12,8 +12,14 @@ Page({
   select: function(e) {
     var local_array = this.data.array;
     var local_roles = new Set(this.data.roles);
-    local_array[e.currentTarget.dataset.id].color = "green";
-    local_roles.add(e.currentTarget.dataset.id)
+    var selected_id = e.currentTarget.dataset.id
+    if (!local_roles.has(selected_id)) {
+      local_array[selected_id].color = "green";
+      local_roles.add(selected_id)
+    } else {
+      local_array[selected_id].color = "blue";
+      local_roles.delete(selected_id)
+    }
     this.setData({
       array: local_array,
       roles: [...local_roles]
@@ -29,56 +35,56 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
